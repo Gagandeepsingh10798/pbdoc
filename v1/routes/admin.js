f048df655650e0aaa7255bf45a87ed8529178aba
Gaurav Sharma <gaurav.sharma@patientbond.com>
2022-11-17 11:57:39 +0530

    add chnages

--- v1/routes/admin.js
+++ v1/routes/admin.js
@@ -11,19 +11,19 @@ On-Boarding
 router.post("/signup",upload.single("user_file"),validations.admin.validateSignup,controllers.admin.signup);
 router.post("/login", validations.admin.validateLogin,controllers.admin.login);
 router.post("/logout", controllers.admin.logout);
-router.post("/forgot/password", validations.admin.validateForgotPassword,permission.checkpermission, controllers.admin.forgotPassword);
+router.post("/forgot/password", validations.admin.validateForgotPassword, controllers.admin.forgotPassword);
 router.post("/reset/password" ,controllers.admin.resetPassword);
-router.post("/change/password", validations.admin.isAdminValid,permission.checkpermission, controllers.admin.changePassword);
-router.get("/profile", validations.admin.isAdminValid,permission.checkpermission, controllers.admin.getProfile);
+router.post("/change/password", validations.admin.isAdminValid, controllers.admin.changePassword);
+router.get("/profile", validations.admin.isAdminValid, controllers.admin.getProfile);
 router.put('/profile',validations.admin.isAdminValid,upload.single("profilePic"),controllers.admin.updateProfile);
 /*
 Client APIs
 */
-router.post('/client',validations.admin.isAdminValid,permission.checkpermission,validations.admin.validateCreateCLient,controllers.admin.createClient);
-router.get('/client',validations.admin.isAdminValid,permission.checkpermission,controllers.admin.getClient);
-router.get('/client/:id',validations.admin.isAdminValid,permission.checkpermission,controllers.admin.getClientbyid);
-router.put('/client/:id',validations.admin.isAdminValid,permission.checkpermission,validations.admin.updateCreateCLient,controllers.admin.updateClient);
-router.delete('/client/:id',validations.admin.isAdminValid,permission.checkpermission,controllers.admin.deleteClient);
+router.post('/client',validations.admin.isAdminValid,validations.admin.validateCreateCLient,controllers.admin.createClient);
+router.get('/client',validations.admin.isAdminValid,controllers.admin.getClient);
+router.get('/client/:id',validations.admin.isAdminValid,controllers.admin.getClientbyid);
+router.put('/client/:id',validations.admin.isAdminValid,validations.admin.updateCreateCLient,controllers.admin.updateClient);
+router.delete('/client/:id',validations.admin.isAdminValid,controllers.admin.deleteClient);
 
 /*
 Module APIs
