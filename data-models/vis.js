9f9fc72186d3d44a6cb4a9a2f49003eee0e7c902
Gaurav Sharma <gaurav.sharma@patientbond.com>
2022-11-23 13:04:10 +0530

    add vis dataModel

---
+++ data-models/vis.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+const Schema = mongoose.Schema;
+const VisModel = new Schema({
+    moduleId: {
+        type: ObjectId,
+        ref: 'modules'
+    },
+    nodes: [{
+        id:String,
+        label:String,
+        title:String,
+        x:String,
+        y:String
+     }] ,
+    edges : [{
+        from : String,
+        to : String
+         }],
+});
+const Vis =mongoose.model('Vis',VisModel);
+module.exports = Vis;
